78d43acdc9c54f978539fe98b4a5683e9f98374c
liangyuRain <liangyuzhaorain@hotmail.com>
2018-03-01 02:25:49 -0800

    minor

--- model.py
+++ model.py
@@ -40,7 +40,7 @@ def run_model():
     # model = OneLineModel(window_num=2, worker_num=5, person_generator=generator)
     # model = OneLinePickupModel(window_num=2, worker_num=5, person_generator=generator)
     # model = MultiLineModel(window_num=2, worker_num=5, person_generator=generator)
-    model = MultiLineOnePickupModel(window_num=2, worker_num=5, person_generator=generator)
+    model = MultiLinePickupModel(window_num=2, worker_num=5, person_generator=generator)
 
     for _ in range(SIMULATION_TIME):
         print(model.print_stat())
