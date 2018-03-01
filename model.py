b01ae020c695a7c8b394b38d9be5d28774bf27c9
liangyu <liangyuzhaorain@hotmail.com>
2018-03-01 02:20:05 -0800

    add models comment

--- model.py
+++ model.py
@@ -37,6 +37,9 @@ def generator(model):
 
 
 def run_model():
+    # model = OneLineModel(window_num=2, worker_num=5, person_generator=generator)
+    # model = OneLinePickupModel(window_num=2, worker_num=5, person_generator=generator)
+    # model = MultiLineModel(window_num=2, worker_num=5, person_generator=generator)
     model = MultiLineOnePickupModel(window_num=2, worker_num=5, person_generator=generator)
 
     for _ in range(SIMULATION_TIME):
